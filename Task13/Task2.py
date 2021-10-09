77b9cb16c819815af82a89c220976b7e795d16f3
Danil <dan.frais2000@gmail.com>
2021-10-09 21:03:16 +0300

    Revert "Task13 is Done"
    This reverts commit 4446a110a471721e9926d9829b0bdf8a74392426.

--- Task13/Task2.py
+++
@@ -1,8 +0,0 @@
-def test1():
-    print("I'm first function")
-    test2()
-
-def test2():
-    return print("I'm second function")
-
-test1()
--- Task13/task1.py
+++
@@ -1,5 +0,0 @@
-def Test():
-    a, b, c, d = 1, 2, 3, 4
-    #__code__.co_nlocals output overthing locals variablse in function
-    return print(Test.__code__.co_nlocals)
-Test()
