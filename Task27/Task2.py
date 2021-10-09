570f900ac287b486bd8b85d275203c0fab050368
Danil <dan.frais2000@gmail.com>
2021-10-09 20:46:11 +0300

    Task27 is Done

---
+++ Task27/Task2.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+
+class Stack:
+    def __init__(self):
+        self._items = None
+
+    def is_empty(self):
+        return bool(self._items)
+
+    def push(self, item):
+        self._items.append(item)
+
+    def pop(self):
+        return self._items.pop()
+
+    def peek(self):
+        return self._items[len(self._items) - 1]
+
+    def size(self):
+        return len(self._items)
+
+    def __repr__(self):
+        representation = "<Stack>\n"
+        for ind, item in enumerate(reversed(self._items), 1):
+            representation += f"{ind}: {str(item)}\n"
+        return representation
